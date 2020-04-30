4cecca4df38d807d39cd59d21ca45cc9ec0fbee0
unknown <s16424@pjwstk.edu.pl>
2020-04-30 22:37:01 +0200

    Cwiczenia 9 - niedokonczone, zrobione 9/12, 01.05 wrzuce dokonczone zadanka, przepraszam

---
+++ LinqConsoleApp/LinqConsoleApp/LinqConsoleApp/Emp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqConsoleApp
+{
+    public class Emp
+    {
+        public int Empno { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+        public DateTime? HireDate { get; set; }
+        public int? Deptno { get; set; }
+        public Emp Mgr { get; set; }
+
+        public override string ToString()
+        {
+            //return " (" + Empno + ") " + Ename + " " + Job + " " + Salary + " " + HireDate + " " + Deptno + " " + Mgr;
+            return Ename + " (" + Empno + ")";
+        }
+        
+    }
+}
